e88cc6a6fbf0e35ab2ffa25cf02f9e31cff4cc3c
cjconnor18 <cjallen1988@gmail.com>
2021-01-27 10:14:30 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const morgan = require('morgan');
+
+
+const app = express();
+
+//set up server
+app.listen(3000);
+
+//set view engine
+app.set('view engine', 'ejs');
+
+//middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+})
+
+//routes
+app.get('/', (req, res) => {
+  res.render('index');
+})
+
+
