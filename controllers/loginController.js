5e9be55d70b4dd11dbc84953d70f240e2eb38b98
cjconnor18 <cjallen1988@gmail.com>
2021-01-28 15:05:40 -0600

    cleaned up white space and made sure listOfPlaylists is still an array

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -3,13 +3,14 @@ const querystring = require('querystring');
 const secrets = require('../secrets');
 const Playlist = require('../models/playlist');
 const generateRandomString = require('../miscFunctions').generateRandomString;
+const { get } = require('request');
 
 
 const client_id = secrets.clientID;
 const client_secret = secrets.clientSecret;
 const redirect_uri = "http://localhost:3000/login/callback";
 let stateKey = 'spotify_auth_state';
-let listOfPlaylists = [];
+let listsOfPlaylists = [];
 
 const login_index = (req, res) => {
   res.render('./login/index', { secrets });
@@ -58,7 +59,7 @@ const login_user = (req, res) => {
       },
       json: true
     };
-
+    
     request.post(authOptions, function(error, response, body) {
       if (!error && response.statusCode === 200) {
 
@@ -70,7 +71,7 @@ const login_user = (req, res) => {
           headers: { 'Authorization': 'Bearer ' + access_token },
           json: true
         };
-
+         
         // use the access token to access the Spotify Web API
         request.get(options, function(error, response, body) {
           // console.log(body);
@@ -82,53 +83,47 @@ const login_user = (req, res) => {
             json: true
           }
 
+          // gets the first 50 playlists (number is set according to limit )
           request.get(optionsPlaylist, (error, response, body) => {
-
-            //console.log(body);
             body.items.forEach(playlist => {
-              listOfPlaylists.push(new Playlist(playlist.id, playlist.name, playlist.description, playlist.tracks.total, playlist.tracks.href));
+              listsOfPlaylists.push(new Playlist(playlist.id, playlist.name, playlist.description, playlist.tracks.total, playlist.tracks.href));
             });
               
-            
-
-
-            if(body.total > limit + offset) {
+            //next playlist using the next url specified in the body
+            if (body.next !== null) {
               request.get({
                 url: body.next,
                 headers: { 'Authorization': 'Bearer ' + access_token },
                 json: true
               }, (error, response, body) => {
-
-                //console.log(body);
-                
+                body.items.forEach(playlist => {
+                  listsOfPlaylists.push(new Playlist(playlist.id, playlist.name, playlist.description, playlist.tracks.total, playlist.tracks.href));
+                });
+                console.log(body);
               })
             }
-
-
           })
-
-
-
-        });
+        }) 
         
         res.redirect('/login/playlists/#' +
           querystring.stringify({
             access_token: access_token,
             refresh_token: refresh_token
-          }));
+        }));  
+    
       } else {
         res.redirect('/#' +
           querystring.stringify({
             error: 'invalid_token'
-          }));
+        }));
       }
     });
-  }
+  } 
 };
 
 const login_playlists_get = (req, res) => {
-  console.log(listOfPlaylists);
-  res.render('./login/playlists', { playlists : listOfPlaylists });
+  console.log(listsOfPlaylists , listsOfPlaylists.length);
+  res.render('./login/playlists', { playlists : listsOfPlaylists });
 }
 
 const login_player = (req, res) => {
