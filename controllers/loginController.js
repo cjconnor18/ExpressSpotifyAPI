810b027ef3b1e604fa45c03c68cb60d885b8bb5b
cjconnor18 <cjallen1988@gmail.com>
2021-02-18 10:49:12 -0600

    refresh token using ajax to get new access token

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -183,9 +183,11 @@ const login_refreshToken = (req, res) => {
 
   request.post(authOptions, function(error, response, body) {
     if (!error && response.statusCode === 200) {
-      var access_token = body.access_token;
+      access_token = body.access_token;
+      
       res.send({
-        'access_token': access_token
+        'access_token': access_token,
+        'refresh_token': refresh_token
       });
     }
   });
